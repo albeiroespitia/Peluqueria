a35616750f6f3e7fd03efd2fc7018ebae7be6e3b
Albeiro <albeiroespitiasierra@gmail.com>
2018-09-12 14:28:55 -0500

    Agrego mas precios

--- js/login.js
+++ js/login.js
@@ -76,4 +76,29 @@ $('.btn_login').click(function(){
   if((userEmail == "admin") && (passwordEmail == "admin")){
     location.href ="./Admin/index.html";
   }
+
+
+  (function() {
+    // We must use JS as we need to select previous
+    // elements which can't be done with CSS.
+    $('.skew-title').children('span').hover((function() {
+      var $el, n;
+      $el = $(this);
+      n = $el.index() + 1;
+      $el.addClass('flat');
+      if (n % 2 === 0) {
+        return $el.prev().addClass('flat');
+      } else {
+        if (!$el.hasClass('last')) {
+          return $el.next().addClass('flat');
+        }
+      }
+    }), function() {
+      return $('.flat').removeClass('flat');
+    });
+  
+  }).call(this);
+  
 })
+
+
